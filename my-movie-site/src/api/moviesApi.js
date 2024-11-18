9bce3f63ff0d7531de237fe284d247aa8586416c
sangho <steve0312@naver.com>
2024-11-18 16:29:59 +0900

    feat: 현재 상영 중인 영화 데이터를 받아와서 5개만 표시

--- my-movie-site/src/api/moviesApi.js
+++ my-movie-site/src/api/moviesApi.js
@@ -2,6 +2,14 @@ import api from "./axios";
 
 const movieApi = {
   getNowPlayingMovies: async () => {
-    const response = await api.get(`/now_playing`);
+    // const response = await api.get(
+    //   "https://api.themoviedb.org/3/movie/now_playing?api_key=69fbb1ae0de2048ebb2bf5da9eb188a9"
+    // );
+    const response = await api.get(
+      `/now_playing?api_key=${import.meta.env.VITE_TMDB_API_KEY}`
+    );
+    return response.data;
   },
 };
+
+export default movieApi;
--- my-movie-site/src/components/Main.jsx
+++ my-movie-site/src/components/Main.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import movieApi from "../api/moviesApi";
 
 export default function Main() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    async function getMovieData() {
+      // 현재 상영 중인 영화의 데이터를 조회
+      const data = await movieApi.getNowPlayingMovies();
+      const results = data.results;
+      // console.log("data : ", data);
+      // console.log("results : ", results);
+
+      const nowPlayingMovies = results.map((result) => {
+        const { id, title } = result;
+
+        return <li key={id}>{title}</li>;
+      });
+
+      setMovieData(nowPlayingMovies);
+    }
+    getMovieData();
+  }, []);
+
   return (
     <>
-      <div>Main</div>
+      <h2>현재 상영 중인 영화</h2>
+      {/* 현재 상영 중인 영화를 5개만 보여줌 */}
+      <ul>{movieData.slice(0, 5)}</ul>
     </>
   );
 }
