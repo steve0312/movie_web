3a733f8c80b9417d8de9d1049af1c4c946941426
sangho <steve0312@naver.com>
2024-11-19 00:11:56 +0900

    feat: 더보기 버튼을 클릭하면 현재 상영 중인 영화 전체를 표시, css flex 설정

--- my-movie-site/src/pages/NowPlayingList.jsx
+++ my-movie-site/src/pages/NowPlayingList.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import movieApi from "../api/moviesApi";
+import { fetchMovieData } from "../utils/movieService";
 
 export default function NowPlayingList() {
-  return <div>NowPlayingList</div>;
+  const [nowPlayingData, setNowPlayingData] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData(movieApi.getNowPlayingMovies, setNowPlayingData);
+  }, []);
+
+  console.log("nowPlayingData : ", nowPlayingData);
+
+  return (
+    <>
+      <div>
+        <h2>현재 상영 중인 영화</h2>
+        <ul className="flex dotNone">{nowPlayingData}</ul>
+      </div>
+    </>
+  );
 }
