fb63c633327f052b31bdbece4ee360be87ea129a
sangho <steve0312@naver.com>
2024-11-18 15:37:28 +0900

    feat: RootLayout에서 Main 분리

--- my-movie-site/src/layouts/RootLayout.jsx
+++ my-movie-site/src/layouts/RootLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 export default function RootLayout() {
   return (
     <>
       <Header></Header>
-      <main>main</main>
+      <Main></Main>
       <footer>footer</footer>
     </>
   );
