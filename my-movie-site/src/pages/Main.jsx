ae72bf385fdc4d2f2659c3c3452e3daf11474e34
sangho <steve0312@naver.com>
2024-11-19 09:49:20 +0900

    refactor: 더보기 버튼을 링크로 변경

--- my-movie-site/src/pages/Main.jsx
+++ my-movie-site/src/pages/Main.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import movieApi from "../api/moviesApi";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { fetchMovieData } from "../utils/movieService";
 
 export default function Main() {
   const [nowPlayingData, setNowPlayingData] = useState([]);
   const [popularData, setPopularData] = useState([]);
   const [topRatedData, setTopRatedData] = useState([]);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     // 현재 상영 중인 영화의 데이터를 조회
@@ -24,36 +22,20 @@ export default function Main() {
     <>
       <div>
         <h2>현재 상영 중인 영화</h2>
-        <button
-          onClick={() => {
-            navigate("/nowPlayingMovies");
-          }}
-        >
-          더보기
-        </button>
-        {/* 현재 상영 중인 영화를 5개만 보여줌 */}
+        <Link to="/nowPlayingMovies">더보기</Link>
+        {/* 현재 상영 중인 영화를 일부만 보여줌 */}
         <ul className="flex dotNone">{nowPlayingData.slice(0, 7)}</ul>
       </div>
       <div>
+        {/* 인기있는 영화를 일부만 보여줌 */}
         <h2>인기있는 영화</h2>
-        <button
-          onClick={() => {
-            navigate("/popularMovies");
-          }}
-        >
-          더보기
-        </button>
+        <Link to="/popularMovies">더보기</Link>
         <ul className="flex dotNone">{popularData.slice(0, 7)}</ul>
       </div>
       <div>
+        {/* 최고 평점 영화를 일부만 보여줌 */}
         <h2>최고 평점 영화</h2>
-        <button
-          onClick={() => {
-            navigate("/topRatedMovies");
-          }}
-        >
-          더보기
-        </button>
+        <Link to="/topRatedMovies">더보기</Link>
         <ul className="flex dotNone">{topRatedData.slice(0, 7)}</ul>
       </div>
     </>
