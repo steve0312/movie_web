dfc77effafc0beb5f4a4bdee2bbbbcf7fcddbc67
sangho <steve0312@naver.com>
2024-11-21 14:16:03 +0900

    feat: 기존에 저장했던 영화인지 체크하는 기능 구현
    영화의 id를 체크해서 기존에 저장했던 영화가 아니라면 store에 추가

--- my-movie-site/src/store/slices/savedMovieSlice.js
+++ my-movie-site/src/store/slices/savedMovieSlice.js
@@ -17,11 +17,15 @@ const savedMovieSlice = createSlice({
         return acc;
       }, {});
 
-      // movieId를 추가로 포함
+      // movieId 상태를 전역에서 관리할 수 있도록 추가
       movieData.movieId = movieId;
 
-      // 변환된 데이터를 Redux state에 추가
-      state.push(movieData);
+      // 이미 저장한 영화인지 확인
+      const isExist = state.find((movie) => movie.movieId === movieId);
+      if (!isExist) {
+        // 기존에 저장한 영화가 아니라면 변환된 데이터를 Redux state에 추가
+        state.push(movieData);
+      }
     },
   },
 });
