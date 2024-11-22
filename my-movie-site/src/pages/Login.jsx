f888ea241ade27f60fe18bea43eb73ba9eda30bc
sangho <steve0312@naver.com>
2024-11-22 13:40:05 +0900

    fix: 로그인 하고 나서 뒤로가기 누르면 로그인 된 상태로 로그인 페이지가 뜨는 버그 수정

--- my-movie-site/src/pages/Login.jsx
+++ my-movie-site/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../store/slices/authSlice";
@@ -8,6 +8,14 @@ export default function Login() {
   const { isLoggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
+  // 로그인 했을 때 뒤로가기 하면 홈으로 이동
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    }
+  }, [isLoggedIn]);
+
+  // 더미데이터로 로그인 처리해서 그냥 아이디, 비밀번호 입력하고 로그인 버튼 누르면 로그인 상태로 전환
   function handleLoginSubmit(e) {
     e.preventDefault();
 
