58af14aa2cc7cf3568a91b08873a7e1854d48abf
sangho <steve0312@naver.com>
2024-11-25 02:03:10 +0900

    style: 주석으로 적었던 코드 삭제

--- my-movie-site/src/pages/MovieCategory.jsx
+++ my-movie-site/src/pages/MovieCategory.jsx
@@ -1,37 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import movieApi from "../api/moviesApi";
-// import { fetchMovieData } from "../utils/movieListService";
-// import { useParams } from "react-router-dom";
-
-// export default function MovieCategory(props) {
-//   const { category } = useParams();
-//   console.log("category : ", category);
-//   console.log("props : ", props);
-
-//   const [movieData, setMovieData] = useState([]);
-
-//   useEffect(() => {
-//     // 카테고리에 맞는 영화의 API를 호출
-//     switch (category) {
-//       case "nowPlaying":
-//         fetchMovieData(movieApi.getNowPlayingMovies, setMovieData);
-//         break;
-//       case "popular":
-//         fetchMovieData(movieApi.getPopularMovies, setMovieData);
-//         break;
-//       case "topRated":
-//         fetchMovieData(movieApi.getTopRatedMovies, setMovieData);
-//     }
-//   }, [category]);
-
-//   return (
-//     <div>
-//       <h2>{category} 영화</h2>
-//       <ul className="flex dotNone">{movieData}</ul>
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState } from "react";
 import movieApi from "../api/moviesApi";
 import { fetchMovieData } from "../utils/movieListService";
