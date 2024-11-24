4621ecbcb844932729927ff5f4ffcd035b756f78
sangho <steve0312@naver.com>
2024-11-24 23:17:47 +0900

    refactor: 상영 중인 영화, 인기있는 영화, 최고 평점 영화 페이지를 하나의 페이지를 재사용하도록 수정
    - 기존에는 상영 중인 영화, 인기있는 영화, 최고 평점 영화 페이지를 각각 만들어서 3개의 페이지가 존재했는데 이것을 하나의 페이지를 재사용하도록 수정
    - 카테고리별 영화 라우팅 경로 수정

--- my-movie-site/src/router/index.jsx
+++ my-movie-site/src/router/index.jsx
@@ -1,9 +1,60 @@
+// 기존에 잘 동작하던 코드
+
+// import { createBrowserRouter } from "react-router-dom";
+// import RootLayout from "../layouts/RootLayout";
+// import Main from "../pages/Main";
+// import NowPlayingList from "../pages/NowPlayingList";
+// import PopularList from "../pages/PopularList";
+// import TopRatedList from "../pages/TopRatedList";
+// import MovieDetail from "../pages/MovieDetail";
+// import Login from "../pages/Login";
+// import MyPage from "../pages/MyPage";
+// import NotFound from "../pages/NotFound";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootLayout />,
+//     errorElement: <NotFound />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Main />,
+//       },
+//       {
+//         path: "/nowPlayingMovies",
+//         element: <NowPlayingList />,
+//       },
+//       {
+//         path: "/popularMovies",
+//         element: <PopularList />,
+//       },
+//       {
+//         path: "/topRatedMovies",
+//         element: <TopRatedList />,
+//       },
+//       {
+//         path: "/movieDetail/:movieId",
+//         element: <MovieDetail />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//       {
+//         path: "/myPage",
+//         element: <MyPage />,
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
+
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import Main from "../pages/Main";
-import NowPlayingList from "../pages/NowPlayingList";
-import PopularList from "../pages/PopularList";
-import TopRatedList from "../pages/TopRatedList";
+import MovieCategory from "../pages/MovieCategory";
 import MovieDetail from "../pages/MovieDetail";
 import Login from "../pages/Login";
 import MyPage from "../pages/MyPage";
@@ -20,16 +71,8 @@ const router = createBrowserRouter([
         element: <Main />,
       },
       {
-        path: "/nowPlayingMovies",
-        element: <NowPlayingList />,
-      },
-      {
-        path: "/popularMovies",
-        element: <PopularList />,
-      },
-      {
-        path: "/topRatedMovies",
-        element: <TopRatedList />,
+        path: "/movie/:category",
+        element: <MovieCategory />,
       },
       {
         path: "/movieDetail/:movieId",
